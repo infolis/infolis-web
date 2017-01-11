7a204211d1e8d6018d69161168b1aff302219e8c
Konstantin Baierer <kba@users.noreply.github.com>
2017-01-11 13:41:05 +0100

    Update infolis-primo.js

--- public/infolis-primo.js
+++ public/infolis-primo.js
@@ -1,26 +1,37 @@
-function addinfolislinks() {
-    var iconstyle = [
-        'background-image: url(http://infolis.github.io/img/logo-circle.png) !important',
-        'background-size: contain'
-    ].join(';');
-    $(".exltabheaderbuttonsendtolist").each(function(_, buttonlist) {
-        var container = $(buttonlist).closest('.exlsummary');
-        console.log(container);
-        var query = 'title=' + encodeuricomponent($('.exllinkedfieldtitle', container).text());
-        var doilabel = $('b:contains("doi")', container);
-        if (doilabel[0]) query = 'id=' + encodeuricomponent(doilabel[0].nextsibling.textcontent.trim());
-        $(".exlbuttonsendtoinfolis", buttonlist).remove();
-        $(buttonlist).append($(`
-          <li class="exlbuttonsendtoinfolis">
-            <a href="http://infolis.gesis.org/search?${query}" target="blank">
-            <span class="exlbuttonsendtolabel">in infolis suchen</span>
-            <span class="exlbuttonsendtoicon" style='${iconstyle}'></span>
-            </a>
-          </li>`));
-    });
+// ==UserScript==
+// @name         Primo Infolis
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        http://primo.bib.uni-mannheim.de/primo_library/libweb/action/*
+// @grant        none
+// ==/UserScript==
+
+function addInfolisLinks() {
+	var iconStyle = [
+		'background-image: url(http://infolis.github.io/img/logo-circle.png) !important',
+		'background-size: contain'
+	].join(';');
+	$(".EXLTabHeaderButtonSendToList").each(function(_, buttonList) {
+		var container = $(buttonList).closest('.EXLSummary');
+		console.log(container);
+		var query = 'title=' + encodeURIComponent($('.EXLLinkedFieldTitle', container).text());
+		var doiLabel = $('b:contains("DOI")', container);
+		if (doiLabel[0]) query = 'id=' + encodeURIComponent(doiLabel[0].nextSibling.textContent.trim());
+		$(".EXLButtonSendToInfolis", buttonList).remove();
+		$(buttonList).append($(`
+		  <li class="EXLButtonSendToInfolis">
+			<a href="http://infolis.gesis.org/search?${query}" target="blank">
+			<span class="EXLButtonSendToLabel">In Infolis suchen</span>
+			<span class="EXLButtonSendToIcon" style='${iconStyle}'></span>
+			</a>
+		  </li>`));
+	});
 }
-function initializeinfolis() {
-    $(document).ajaxComplete(addinfolislinks);
-    addinfolislinks();
+function initializeInfolis() {
+	$(document).ajaxComplete(addInfolisLinks);
+	addInfolisLinks();
 }
-initializeinfolis();
+
+initializeInfolis();
